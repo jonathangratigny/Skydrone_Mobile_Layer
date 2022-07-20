e9141d61de699bb404c988a88160e3c7e7413dfb
TheoD-LHP5 <theo.demarle@live.fr>
2022-07-20 14:11:48 +0200

    Add style Android

--- src/screens/Stock.js
+++ src/screens/Stock.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList, Alert, Modal, Pressable, TouchableOpacity } from 'react-native';
+import { Platform, View, Text, StyleSheet, FlatList, Alert, Modal, Pressable, TouchableOpacity } from 'react-native';
 import { BASE_URL } from "../config";
 import { useAuth } from "../context/AuthContext";
 
@@ -145,168 +145,186 @@ const StockPage = () =>{
 }
 
 const styles = StyleSheet.create({
-    centeredView:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 22
-    },
-    modalView:{
-        margin: 20,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 35,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-    },
-    button:{
-        borderRadius: 20,
-        padding: 15,
-        marginTop: 50,
-        elevation: 2,
-    },
-    buttonClose:{
-        backgroundColor: "#2196F3",
-    },
-    buttonToSAV:{
-        backgroundColor: "firebrick",
-    },
-    buttonToStock:{
-        backgroundColor: "forestgreen",
-    },
-    textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-    },
-    modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-    },
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-    },
-    cell:{
-        width: 425,
-    },
-    titleStock:{
-        marginBottom: 20,
-        marginTop: 5,
-        padding: 10,
-        borderRadius: 15,
-        color: "#3caae9",
-        letterSpacing: 2,
-        textAlign: "center",
-        fontSize: 18,
-        textTransform: "uppercase"
-    },
-    titleSky:{
-        marginBottom: 10,
-        marginTop: 10,
-        padding: 10,
-        borderRadius: 15,
-        color: "#3caae9",
-        letterSpacing: 2,
-        textAlign: "center",
-        fontSize: 30,
-        fontWeight: "bold",
-    },
-    titleBlack:{
-        color: 'black'
-    },
-    textStateDrone:{
-        padding: 10,
-        letterSpacing: 1,
-        fontSize: 18,
-        textAlign: "center",
-        color: "#32cd32",
-        fontWeight: "bold"
-    },
-    textStateDroneUnavailable:{
-        color: '#dc143c',
-        padding: 10,
-        letterSpacing: 1,
-        fontSize: 18,
-        textAlign: "center",
-        fontWeight: "bold"
-    },
-    textStateDroneResa:{
-        color: '#ff8c00',
-        padding: 10,
-        letterSpacing: 1,
-        fontSize: 18,
-        textAlign: "center",
-        fontWeight: "bold"
-    },
-    textState:{
-        padding: 10,
-        letterSpacing: 1,
-        fontSize: 18,
-        textAlign: "center",
-    },
-    textStateModal:{
-        padding: 5,
-        letterSpacing: 1,
-        textAlign: "center",
-    },
-    card:{
-        padding: 25,
-        margin: 20,
-        borderRadius:20,
-        borderWidth: 1,
-    },
-    listWrapper:{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        borderBottomWidth: 1,
-    },
-    text:{
-        flex: 1,
-        fontSize: 14,
-        textAlign: 'center',
-        textTransform: 'capitalize',
-        padding: 10,
-
-    },
-    textDroneName:{
-        flex: 1,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        textTransform: 'uppercase',
-        padding: 10,
-    },
-    textDroneNameModal:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        textTransform: 'uppercase',
-        padding: 10
-    },
-    title: {
-        marginBottom: 20,
-        marginTop: 20,
-        padding: 10,
-        borderRadius: 15,
-        color: "#3caae9",
-        letterSpacing: 2,
-        textAlign: "center",
-        fontSize: 20,
-        fontWeight: "bold",
-        textTransform: 'uppercase'
-    },
     containerBtn: {
         alignSelf: "center"
-    }
+    },
+    ...Platform.select({
+        ios:{
+            centeredView:{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 22,
+            },
+            modalView:{
+                margin: 20,
+                backgroundColor: "white",
+                borderRadius: 20,
+                padding: 35,
+                alignItems: "center",
+                shadowColor: "#000",
+                shadowOffset: {
+                    width: 0,
+                    height: 2
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 4,
+                elevation: 5
+            },
+            button:{
+                borderRadius: 20,
+                padding: 15,
+                marginTop: 50,
+                elevation: 2,
+            },
+            buttonClose:{
+                backgroundColor: "#2196F3",
+            },
+            buttonToSAV:{
+                backgroundColor: "firebrick",
+            },
+            buttonToStock:{
+                backgroundColor: "forestgreen",
+            },
+            textStyle: {
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center"
+            },
+            modalText: {
+                marginBottom: 15,
+                textAlign: "center"
+            },
+            container: {
+                flex: 1,
+                backgroundColor: "#fff",
+                alignItems: "center",
+            },
+            cell:{
+                width: 425,
+            },
+            titleStock:{
+                marginBottom: 20,
+                marginTop: 5,
+                padding: 10,
+                borderRadius: 15,
+                color: "#3caae9",
+                letterSpacing: 2,
+                textAlign: "center",
+                fontSize: 18,
+                textTransform: "uppercase",
+            },
+            titleSky:{
+                marginBottom: 10,
+                marginTop: 10,
+                padding: 10,
+                borderRadius: 15,
+                color: "#3caae9",
+                letterSpacing: 2,
+                textAlign: "center",
+                fontSize: 30,
+                fontWeight: "bold",
+            },
+            titleBlack:{
+                color: 'black'
+            },
+            textStateDrone:{
+                padding: 10,
+                letterSpacing: 1,
+                fontSize: 18,
+                textAlign: "center",
+                color: "#32cd32",
+                fontWeight: "bold"
+            },
+            textStateDroneUnavailable:{
+                color: '#dc143c',
+                padding: 10,
+                letterSpacing: 1,
+                fontSize: 18,
+                textAlign: "center",
+                fontWeight: "bold"
+            },
+            textStateDroneResa:{
+                color: '#ff8c00',
+                padding: 10,
+                letterSpacing: 1,
+                fontSize: 18,
+                textAlign: "center",
+                fontWeight: "bold"
+            },
+            textState:{
+                padding: 10,
+                letterSpacing: 1,
+                fontSize: 18,
+                textAlign: "center",
+            },
+            textStateModal:{
+                padding: 5,
+                letterSpacing: 1,
+                textAlign: "center",
+            },
+            card:{
+                padding: 25,
+                margin: 20,
+                borderRadius:20,
+                borderWidth: 1,
+            },
+            listWrapper:{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                borderBottomWidth: 1,
+            },
+            text:{
+                flex: 1,
+                fontSize: 14,
+                textAlign: 'center',
+                textTransform: 'capitalize',
+                padding: 10,
+        
+            },
+            textDroneName:{
+                flex: 1,
+                fontSize: 18,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                textTransform: 'uppercase',
+                padding: 10,
+            },
+            textDroneNameModal:{
+                fontSize: 20,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                textTransform: 'uppercase',
+                padding: 10
+            },
+            title: {
+                marginBottom: 20,
+                marginTop: 20,
+                padding: 10,
+                borderRadius: 15,
+                color: "#3caae9",
+                letterSpacing: 2,
+                textAlign: "center",
+                fontSize: 20,
+                fontWeight: "bold",
+                textTransform: 'uppercase'
+            }
+        },
+        android:{
+            textDroneName:{
+                flex: 1,
+                fontSize: 18,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                textTransform: 'uppercase',
+                padding: 10,
+            },
+        },
+        default:{
+            //
+        }
+
+    })
 })
 
 export default StockPage;
